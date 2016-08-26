93b26aec1ef3fc09877ff898238fca030acb24f7
Arley <arley.duarte@zyght.com>
2016-08-26 16:34:26 -0300

    start

---
+++ app/src/main/java/android/amdp/com/guihelpers/MultiSpinner.java
@@ -0,0 +1,11 @@
+package android.amdp.com.guihelpers;
+
+/**
+ * Created by arley on 8/26/16.
+ */
+public class MultiSpinner {
+
+    public String getMessage(){
+        return  "Infor del pro";
+    }
+}
