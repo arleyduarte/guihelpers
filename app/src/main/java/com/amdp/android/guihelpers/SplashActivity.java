bd8d964348bc7cdb6f2607d56f49c9035115007e
Arley <arley.duarte@zyght.com>
2016-09-22 19:19:59 -0300

    splash

--- app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
+++ app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
@@ -2,14 +2,19 @@ package com.amdp.android.guihelpers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public  abstract  class SplashActivity extends AppCompatActivity {
 
     private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
     }
 }
