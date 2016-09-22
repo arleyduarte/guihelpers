b1a2f334a88129f88d99f24c5ddf3a72696bfb19
Arley <arley.duarte@zyght.com>
2016-09-22 19:24:37 -0300

    splash

--- app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
+++ app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.amdp.android.guihelpers;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -7,7 +9,7 @@ import android.view.WindowManager;
 
 public  abstract  class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_SCREEN_DELAY = 3000;
+    private static final long SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,27 @@ public  abstract  class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+    }
+
+
+    protected void showNextActivity(final Class<?> cls){
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, cls);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
