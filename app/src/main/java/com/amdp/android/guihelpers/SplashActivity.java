437a9214c0254ea6768ce10e36fd2cb662d856af
Arley <arley.duarte@zyght.com>
2016-09-22 19:13:51 -0300

    splash

--- app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
+++ app/src/main/java/com/amdp/android/guihelpers/SplashActivity.java
@@ -11,4 +11,5 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-   
+    }
+}
