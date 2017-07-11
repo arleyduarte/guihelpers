df82da5468d4d4f3f84ce0e4b329e3da3247aaa7
Arley <arley.duarte@zyght.com>
2017-07-11 17:07:40 -0400

    pah

--- app/src/main/java/com/amdp/android/guihelpers/utils/FileUtils.java
+++ app/src/main/java/com/amdp/android/guihelpers/utils/FileUtils.java
@@ -32,6 +32,9 @@ public class FileUtils {
 
 
     public static String getPath(Context context, Uri uri) {
+
+        String path = "";
+
         if ("content".equalsIgnoreCase(uri.getScheme())) {
             String[] projection = { "_data" };
             Cursor cursor = null;
@@ -40,16 +43,16 @@ public class FileUtils {
                 cursor = context.getContentResolver().query(uri, projection, null, null, null);
                 int column_index = cursor.getColumnIndexOrThrow("_data");
                 if (cursor.moveToFirst()) {
-                    return cursor.getString(column_index);
+                    path=  cursor.getString(column_index);
                 }
             } catch (Exception e) {
 
             }
         }
         else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
+            path= uri.getPath();
         }
 
-        return null;
+       return path;
     }
 }
